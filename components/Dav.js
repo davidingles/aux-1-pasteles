597abbd4746df08eee6742575c014c1f6acce2a5
davidingles <davidingleslopez@gmail.com>
2023-04-05 13:51:03 +0200

    resscalo pasteles

--- components/Dav.js
+++ components/Dav.js
@@ -6,71 +6,74 @@ import React, { useRef } from "react";
 import { useGLTF } from "@react-three/drei";
 
 export function Dav(props) {
-  const { nodes, materials } = useGLTF("/5D ROINTE MPCASPT.gltf");
+  const { nodes, materials } = useGLTF("/3D PASTELES.gltf");
   return (
     <group {...props} dispose={null}>
-      <group position={[-0.15, 0.02, 0.02]}>
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes["TQ_ARD-geom"].geometry}
-          material={materials.TQ_ARD_front}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes["TQ_ARD-geom_1"].geometry}
-          material={materials.TQ_ARD_back}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes["TQ_ARD-geom_2"].geometry}
-          material={materials.TQ_ARD_edge}
-        />
-      </group>
-      <group position={[0.01, 0.02, 0.14]}>
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes["TQ_ARD-geom002"].geometry}
-          material={materials.TQ_ARD_front}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes["TQ_ARD-geom002_1"].geometry}
-          material={materials.TQ_ARD_back}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes["TQ_ARD-geom002_2"].geometry}
-          material={materials.TQ_ARD_edge}
-        />
-      </group>
-      <group position={[0.08, 0.04, -0.03]}>
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes["TQ_ARD-geom001"].geometry}
-          material={materials["TQ_ARD_front.001"]}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes["TQ_ARD-geom001_1"].geometry}
-          material={materials["back-dil"]}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes["TQ_ARD-geom001_2"].geometry}
-          material={materials["TQ_ARD_edge.001"]}
-        />
-      </group>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Plano001.geometry}
+        material={materials["Material.001"]}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Plano002.geometry}
+        material={materials["Material.001"]}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Plano003.geometry}
+        material={materials["Material.001"]}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Texto.geometry}
+        material={materials["Material.001"]}
+        position={[0.49, 0, 0.06]}
+        rotation={[Math.PI / 2, 0, -Math.PI / 2]}
+        scale={0.5}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Texto001.geometry}
+        material={materials["Material.001"]}
+        position={[0.03, 0, 0.4]}
+        rotation={[Math.PI / 2, 0, -Math.PI / 2]}
+        scale={0.5}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Texto002.geometry}
+        material={materials["Material.001"]}
+        position={[0.44, 0.04, -0.25]}
+        rotation={[Math.PI / 2, 0, -Math.PI / 2]}
+        scale={0.5}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes["TQ_ARD-geom"].geometry}
+        material={materials.TQ_ARD_front}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes["TQ_ARD-geom_1"].geometry}
+        material={materials["back-dil"]}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes["TQ_ARD-geom_2"].geometry}
+        material={materials.TQ_ARD_edge}
+      />
     </group>
   );
 }
 
-useGLTF.preload("/5D ROINTE MPCASPT.gltf");
+useGLTF.preload("/3D PASTELES.gltf");
--- pages/index.tsx
+++ pages/index.tsx
@@ -70,8 +70,8 @@ const Home: NextPage = () => {
             
               <Dav 
                 // className={styles.dav}
-                scale={1.1}
-                position={[-.0,0.07,0.]}
+                scale={0.5}
+                position={[-.0,0.1,0.]}
                 rotation={[Math.PI / 1000, 0, 0]}
               />
           
